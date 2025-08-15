adfba7179ace795bac74747faec06726a2e8c13f
Romain Ardiet <contact@romainardiet.com>
2025-08-15 12:14:32 +0100

    Add ParquetPartitionTest

---
+++ library/src/test/java/io/github/romibuzi/parquetdiff/metadata/ParquetPartitionTest.java
@@ -0,0 +1,14 @@
+package io.github.romibuzi.parquetdiff.metadata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParquetPartitionTest {
+    @Test
+    void getKeyAndValue() {
+        ParquetPartition partition = new ParquetPartition("date", "2022-03-26");
+        assertEquals("date", partition.getKey());
+        assertEquals("2022-03-26", partition.getValue());
+    }
+}
